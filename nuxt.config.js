03b980bfc34ca39bb8ff6ac2988e3e6253d333b2
mehuliate <darksideofbotix@gmail.com>
2020-10-21 17:00:55 +0700

    finished get started frontend

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,15 +9,31 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js'
+      },
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js'
+      },
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js'
+      }
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    '@/assets/scss/main.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~plugins/vform'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -25,18 +41,36 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    '@nuxtjs/router'
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
+
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth'
   ],
+
+  auth: {
+  strategies: {
+    local: {
+      endpoints: {
+        login: { url: '/login', method: 'post', propertyName: 'token' },
+        logout: { url: '/logout', method: 'post' },
+        user: { url: '/me', method: 'get', propertyName: 'data' }
+      },
+      // tokenRequired: true,
+      // tokenType: 'bearer',
+      // globalToken: true,
+      // autoFetchUser: true
+    }
+  }
+},
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
+  axios: {
+    baseUrl:process.env.API_URL
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
---
+++ pages/auth/register.vue
@@ -0,0 +1,92 @@
+<template>
+    <section class="authentication">
+        <div class="auth-body">
+            <h1 class="text-uppercase fw-500 mb-4 text-center font-22">
+                Register
+            </h1>
+            <form class="auth-form" @submit.prevent="submit">
+                <div class="form-group">
+                    <input
+                        type="text"
+                        v-model.trim="form.name"
+                        name="name"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Full Name"
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="text"
+                        v-model.trim="form.username"
+                        name="username"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Username"
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="text"
+                        v-model.trim="form.email"
+                        name="email"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Email"
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        v-model.trim="form.password"
+                        name="password"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Password"
+                    />
+                </div>
+                <div class="form-group">
+                    <input
+                        type="password"
+                        v-model.trim="form.password_confirmation"
+                        name="password_confirmation"
+                        class="form-control form-control-lg font-14 fw-300"
+                        placeholder="Confirm Password"
+                    />
+                </div>
+                
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary primary-bg-color font-16 fw-500 text-uppercase">
+                        Register
+                    </button>
+                </div>
+                <p class="font-14 fw-400 text-center mt-4">
+                    Already have an account?
+                    <a class="color-blue" href="#"> Login</a>
+                </p>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+import Form from 'vform'
+export default {
+    data(){
+        return {
+            form: new Form({
+                username: 'ssss',
+                name:'',
+                email: '',
+                password: '',
+                password_confirmation:''
+            })
+        }
+    },
+    methods: {
+        submit(){
+            console.log('submitt');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
